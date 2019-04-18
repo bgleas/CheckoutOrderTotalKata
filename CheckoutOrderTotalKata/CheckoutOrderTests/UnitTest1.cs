54c8d74287ba263ab47d244fd0783eb6e2052ff7
bgleas <bgleas@umich.edu>
2019-04-17 21:01:46 -0500

    Test 2: AcceptMultipleScannedItemsAndHaveCorrectTotal
    Create test initialize and new test

--- CheckoutOrderTotalKata/CheckoutOrderTests/UnitTest1.cs
+++ CheckoutOrderTotalKata/CheckoutOrderTests/UnitTest1.cs
@@ -6,15 +6,39 @@ namespace CheckoutOrderTests
     [TestClass]
     public class UnitTest1
     {
+        CheckOutSystem checkOutSystem;
+
+        [TestInitialize]
+        public void InitalizeCatalogOfItems()
+        {
+            checkOutSystem = new CheckOutSystem();
+
+            checkOutSystem.AddItem("Soup", 1.99);
+            checkOutSystem.AddItem("Ice Cream", 4.50);
+            checkOutSystem.AddItem("Candy", 0.75);
+
+            checkOutSystem.AddItem("Apples", 1.25);
+            checkOutSystem.AddItem("Pears", 1.50);
+            checkOutSystem.AddItem("Oranges", 1.00);
+        }
+
         [TestMethod]
         public void AcceptScannedItemAndHaveCorrectTotal()
         {
-            CheckOutSystem checkOutSystem = new CheckOutSystem();
-            checkOutSystem.AddItem("Candy", 0.75);
-
             checkOutSystem.Scan("Candy");
 
             Assert.AreEqual(0.75, checkOutSystem.GetTotal());
+        }
+
+        [TestMethod]
+        public void AcceptMultipleScannedItemsAndHaveCorrectTotal()
+        {
+            checkOutSystem.Scan("Candy");
+            checkOutSystem.Scan("Candy");
+            checkOutSystem.Scan("Ice Cream");
+            checkOutSystem.Scan("Soup");
+
+            Assert.AreEqual(7.99, checkOutSystem.GetTotal());
         }
     }
 }
